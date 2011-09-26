11980b19e2920d2cff31fdfe588916b26c7063f6
Vikash <vik1987@gmail.com>
2011-09-26 15:28:25 +0530

    Post Page Updated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -88,5 +88,9 @@ class CategoriesController < ApplicationController
     @categories = Category.all
     @category = Category.find(params[:id])
     @category.destroy
+   respond_to do |format|
+      format.html { redirect_to categories_url }
+      format.json { head :ok }
+    end
   end
 end
